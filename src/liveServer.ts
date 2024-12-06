dc9f456f65dc09a4e95238097931758b11116157
Wd-Endd <endercitihacker11111@gmail.com>
2024-12-06 14:59:43 +0700

    ;-;

--- src/liveServer.ts
+++ src/liveServer.ts
@@ -1,10 +1,34 @@
-import express from 'express';
+import { exec } from 'child_process';
+import express from "express";
 
 export function liveServer(destination: string, port: number = 5500) {
   const app = express();
 
+  let url: string;
   app.use(express.static(destination));
   app.listen(port, () => {
-    console.log(`Server hosted on http://localhost:${port}!`);
+    url = `http://localhost:${port}`;
+    console.log(`Server hosted on ${url}!`);
+
+    tryOpenUrl(url);
   });
+
+  function tryOpenUrl(url: string) {
+    const commands = [ 'xdg-open', 'open', 'start', 'termux-open-url'];
+
+    for (const cmd of commands) {
+      exec(`${cmd} ${url}`, (error, stdout, stderr) => {
+        if (error) {
+          // console.error(`Lỗi khi thực thi lệnh ${cmd}: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          // console.error(`stderr: ${stderr}`);
+          return;
+        }
+        // console.log(`URL mở thành công với lệnh ${cmd}: ${stdout}`);
+      });
+      break;
+    }
+  }
 }
